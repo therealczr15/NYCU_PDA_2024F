ff6d056b79007e7fe8589b9dee7ba8d8efc9322b
therealczr <125191151+therealczr15@users.noreply.github.com>
2025-02-21 14:18:55 +0800

    Update main.cpp

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -7,15 +7,5 @@ int main(int argc, char* argv[])
     rt.Dijkstra(argv[4], argv[5]);
     rt.Layout(argv[4], argv[6]);
     
-    /*fstream in, out;
-    in.open(argv[4]);
-    out.open(argv[5], ios::out);
-    string line;
-    
-    while(getline(in, line))
-    {
-        out << line << endl;
-    }*/
-    
     return 0;
 }
